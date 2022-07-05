5a57de2cff4b7272a09bb8523db45e55a8885502
AdinRC <93084433+AdinRC@users.noreply.github.com>
2022-07-05 22:16:45 +0800

    done for views

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Adin', $pekerjaan = 'Developer')
+class About extends Controller{
+    public function index($nama = 'Adin', $pekerjaan = 'Developer', $umur = 23)
     {
-        echo "hallo nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index' , $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+
+    <h1>About me</h1>
+    <p>Halo, Nama saya <?= $data['nama'];?>,<br> 
+    umur saya <?= $data['umur'];?> tahun,<br> 
+    saya adalah seorang <?= $data['pekerjaan'];?>.</p>
